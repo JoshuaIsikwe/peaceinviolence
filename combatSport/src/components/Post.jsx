c43d146e9836e78abf8c66df21e144ad3657b67c
JoshuaIsikwe <IsikweJoshua@gmail.com>
2024-06-13 20:16:24 +0200

    First commit

--- combatSport/src/components/Post.jsx
+++ combatSport/src/components/Post.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Post = () => {
   return (
-    <div className='w-[36.375rem] min-h-[20rem] bg-[#FEF7FF] rounded-xl border border-[#CAC4D0] my-[1rem]'></div>
+    <div className='w-[36.375rem] min-h-[20rem] bg-[#FEF7FF] rounded-xl border border-[#CAC4D0] my-[1rem]'>
+      <div className='flex gap-5' ><h1>Title</h1><span className='flex justify-end'>6th June 2024</span></div>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus fuga, laborum magni nobis obcaecati sit at non animi quasi iste.</p>
+    </div>
   );
 };
 
--- combatSport/src/pages/CreatePost.jsx
+++ combatSport/src/pages/CreatePost.jsx
@@ -119,3 +119,4 @@ const CreatePost = () => {
 };
 
 export default CreatePost;
+ 
